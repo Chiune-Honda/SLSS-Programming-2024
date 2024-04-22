46d19023f8f721f881123a3e57ebc825c537e262
Chiune <chiunehonda@gmail.com>
2024-04-22 12:23:28 -0700

    io;

--- notes-2-5-classes-and-objects.py
+++ notes-2-5-classes-and-objects.py
@@ -74,54 +74,60 @@ class Pikachu(Pokemon):
         return response
 
 
-def main():
-    # Create two Pokemon
+class Greninja(Pokemon):
+    def __init__(self, name="Greninja"):
+        super().__init__()
+        self.name = name
+        self.id = 658
+        self.type =["Water","Dark"]
+        self.actual_cry = "GRAAAAAA!"
 
+    def hydropump(self, defender: Pokemon) -> str:
+        response = f"{self.name} used hydropump on {defender.name}."
+        if defender.type.lower() == "electric":
+           response = response + " It was super effective."
+
+        return response
+
+def main():
+
+    # Create two Pokemon
     # Create something 'Pikachu'-like
     pokemon_one = Pokemon()
-
     # Access the properties inside pokemon_one
     # Print the name of pokemon_one
     print(pokemon_one.name)
     print(pokemon_one.type)
-
     # Change the values of the propeties
     pokemon_one.name = "Pikachu"
     pokemon_one.type = "Electric"
     pokemon_one.id = 25
-
     # Print the values from pokemon_one
     print(pokemon_one.name)
     print(pokemon_one.type)
-
     # TODO: Create something 'Squirtle'-like
     #  - Create a new Pokemon object
     #      - Store this in variable pokemon_two
     #  - Squirtle's Pokedex id is 4
     #  - Squirtle's type is Water
-
     pokemon_two = Pokemon()
     pokemon_two.name = "Squirtle"
     pokemon_two.type = "Water"
     pokemon_two.id = 4
-
     # To test, print out all of squirtle's properties
     print(pokemon_two.name)
     print(pokemon_two.id)
     print(pokemon_two.type)
-
     # Test Pokemon cry
     pokemon_one.actual_cry = "Pikachu"
     print(pokemon_one.cry())
     pokemon_two.actual_cry = "Grraaaaaoooorrr"
     print(pokemon_two.cry())
-
     # Test Pokemon consume
     print(pokemon_one.consume("berry"))
     print(pokemon_one.consume("potion"))
     print(pokemon_one.consume("poison"))  # mr. ubial doesn't condone
     print(pokemon_two.consume("berry"))
-
     # Create a new Pikachu object
     pikachu_one = Pikachu()
     # Print name, type, and id of pikachu_one
@@ -129,10 +135,36 @@ def main():
     # Use the Pokemon methods on Pikachu object
     print(pikachu_one.cry())
     print(pikachu_one.consume("berry"))
-
     # Use the thunder method on pokemon_two
     print(pikachu_one.thunder(pokemon_one))
     print(pikachu_one.thunder(pokemon_two))
+    
+    
+    pokemon_three = Pokemon()
+    pokemon_three.name = "Greninja"
+    pokemon_three.type = ["Water","Dark"]
+    pokemon_three.id = 658
+    print(pokemon_three.name)
+    print(pokemon_three.id)
+    print(", ".join(pokemon_three.type))
+    pokemon_three.actual_cry = "GRAAAAAAA"
+    print(pokemon_three.cry())
+    print(pokemon_three.consume("berry"))
+    print(pokemon_three.consume("potion"))
+    print(pokemon_three.consume("poison"))
+    # Create a new Pikachu object
+    greninja_one = Greninja()
+    # Print name, type, and id of pikachu_one
+    print(greninja_one.name, ", ".join(greninja_one.type), greninja_one.id)
+    # Use the Pokemon methods on Pikachu object
+    print(greninja_one.cry())
+    print(greninja_one.consume("berry"))
+
+    # Use the thunder method on pokemon_two
+    print(greninja_one.hydropump(pokemon_one))
+    print(greninja_one.hydropump(pokemon_two))
+
+
 
 
 if __name__ == "__main__":
