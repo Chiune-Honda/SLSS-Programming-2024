be77cbe34f1924bdbb228eb260a11c9d10b8b4a6
Chiune <chiunehonda@gmail.com>
2024-06-05 15:00:34 -0700

    k/l;/

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
 
-WIDTH = 720
-HEIGHT = 1000
+WIDTH = 1080
+HEIGHT = 720
 SCREEN_SIZE = (WIDTH, HEIGHT)
 # Set up the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -32,9 +32,35 @@ class PlayerL(pg.sprite.Sprite):
 
         self.rect.x, self.rect.y = 50, HEIGHT // 2 - 50
 
-        # playerleft = pygame.Rect(50, HEIGHT // 2 - 50, 50, 100)
+        self.change_x = 0
+        self.change_y = 0
 
-        # self.rect = playerleft.get_rect()
+    def go_left(self):
+            """ Called when the user hits the left arrow. """
+            self.change_x = -6        
+    
+    def go_right(self):
+            """ Called when the user hits the right arrow. """
+            self.change_x = 6
+
+    def go_up(self):
+            """ Called when the user hits the left arrow. """
+            self.rect.y -= 2
+    
+    def go_down(self):
+            """ Called when the user hits the right arrow. """
+            self.rect.y += 2
+    
+    def stop(self):
+            """ Called when the user lets off the keyboard. """
+            self.change_x = 0
+
+    def update(self):
+        """Keep the player in the screen"""
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
 
 
 class PlayerR(pg.sprite.Sprite):
@@ -50,10 +76,36 @@ class PlayerR(pg.sprite.Sprite):
 
         self.rect.x, self.rect.y = WIDTH - 100, HEIGHT // 2 - 50
 
-        # Draw a circle inside of it
-        # playerright = pygame.Rect(WIDTH - 100, HEIGHT // 2 - 50, 50, 100)
+        self.change_x = 0
+        self.change_y = 0
 
-        # self.rect = playerright.get_rect()
+    def go_left(self):
+            """ Called when the user hits the left arrow. """
+            self.change_x = -6
+    
+    def go_right(self):
+            """ Called when the user hits the right arrow. """
+            self.change_x = 6
+
+    def go_up(self):
+            """ Called when the user hits the left arrow. """
+            self.rect.y -= 2
+    
+    def go_down(self):
+            """ Called when the user hits the right arrow. """
+            self.rect.y += 2
+
+    def stop(self):
+            """ Called when the user lets off the keyboard. """
+            self.change_x = 0
+
+    def update(self):
+        """Keep the player in the screen"""
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
 
 
 
@@ -84,25 +136,22 @@ class Ball(pg.sprite.Sprite):
     #         ball_speed[0] *= -1
 
         # Ball collision with walls
-        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
-            self.ball_speed[1] *= -1
-        if self.rect.left <= 0 or self.rect.right >= WIDTH:
-            self.ball_speed[0] *= -1
+   
 
 
-        playerleft = PlayerL()
-        playerright = PlayerR()
+        # playerleft = PlayerL()
+        # playerright = PlayerR()
 
-        all_sprites = pg.sprite.Group()
-        ball_sprites = pg.sprite.Group()
+        # all_sprites = pg.sprite.Group()
+        # ball_sprites = pg.sprite.Group()
 
-        all_sprites.add(playerleft)
-        all_sprites.add(playerright)
+        # all_sprites.add(playerleft)
+        # all_sprites.add(playerright)
 
-        ball = Ball()
+        # ball = Ball()
 
-        all_sprites.add(ball)
-        ball_sprites.add(ball)
+        # all_sprites.add(ball)
+        # ball_sprites.add(ball)
 
 # Game loop
 def start():
@@ -128,66 +177,93 @@ def start():
         all_sprites.add(ball)
         ball_sprites.add(ball)
 
+        active_sprite_list = pg.sprite.Group()
+        # active_sprite_list.add(playerleft)
+        # active_sprite_list.add(playerright)
+        
         while not done:
         # --- Event Listener
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     done = True
 
-                    if event.type == pg.K_DOWN:
-                        playerright.go_down()
-                    if event.key == pg.K_LEFT:
-                        playerright.go_left()
-                    if event.key == pg.K_RIGHT:
-                        playerright.go_right()
-                    if event.key == pg.K_UP:
-                        playerright.go_up()
+                # if event.type == pg.KEYDOWN:
+                #     if event.key == pg.K_DOWN:
+                #         playerright.go_down()
+                #     if event.key == pg.K_LEFT:
+                #         playerright.go_left()
+                #     if event.key == pg.K_RIGHT:
+                #         playerright.go_right()
+                #     if event.key == pg.K_UP:
+                #         playerright.go_up()
  
-                    if event.type == pg.K_w:
-                        playerleft.go_up()
-                    if event.key == pg.K_a:
-                        playerright.go_left()
-                    if event.key == pg.K_RIGHT:
-                        playerright.go_right()
-                    if event.key == pg.K_UP:
-                        playerright.jump()
+                #     # if event.key== pg.K_w:
+                #     #     playerleft.go_up()
+                #     # if event.key == pg.K_a:
+                #     #     playerright.go_left()
+                #     # if event.key == pg.K_d:
+                #     #     playerright.go_right()
+                #     # if event.key == pg.K_s:
+                #     #     playerright.go_down()
 
-                if event.type == pg.KEYUP:
-                if event.key == pg.K_LEFT and player.change_x < 0:
-                    player.stop()
-                if event.key == pg.K_RIGHT and player.change_x > 0:
-                    player.stop()
+                # if event.type == pg.KEYUP:
+                #     if event.key == pg.K_LEFT and playerleft.change_x < 0:
+                #         playerleft.stop()
+                #     if event.key == pg.K_RIGHT and playerleft.change_x > 0:
+                #         playerleft.stop()
+                #     if event.key == pg.K_DOWN and playerleft.change_y < 0:
+                #         playerleft.stop()
+                #     if event.key == pg.K_UP and playerleft.change_y > 0:
+                #         playerleft.stop()
+                
+                # if event.type == pg.KEYUP:
+                #     if event.key == pg.K_LEFT and playerright.change_x < 0:
+                #         playerright.stop()
+                #     if event.key == pg.K_RIGHT and playerright.change_x > 0:
+                #         playerright.stop()
+                #     if event.key == pg.K_DOWN and playerright.change_y < 0:
+                #         playerright.stop()
+                #     if event.key == pg.K_UP and playerright.change_y > 0:
+                #         playerright.stop()
+                    
+            # Key and controls
+            keys_pressed = pg.key.get_pressed()
 
-        # Move players
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            playerleft.rect.y -= 5
-        if keys[pygame.K_s]:
-            playerleft.rect.y += 5
-        if keys[pygame.K_UP]:
-            playerright.rect.y -= 5
-        if keys[pygame.K_DOWN]:
-            playerright.rect.y += 5
-        
-        all_sprites.update()
-
-        # Ball collision with players    
-        ball_collided = pg.sprite.spritecollide(playerleft, ball_sprites, False)
-        ball_collided = pg.sprite.spritecollide(playerright, ball_sprites, False)
-
-        screen.fill(WHITE)
+            if keys_pressed[pg.K_w]:
+                playerleft.go_up()
+            if keys_pressed[pg.K_s]:
+                playerleft.go_down()
 
 
-     
-        screen.fill(BLACK)
+            if keys_pressed[pg.K_UP]:
+                playerright.go_up()
+            if keys_pressed[pg.K_DOWN]:
+                playerright.go_down()
 
-        all_sprites.draw(screen)
 
-        # Update the screen with anything new
-        pg.display.flip()
+            all_sprites.update()
 
-        # --- Tick the Clock
-        clock.tick(60)  # 60 fps
+
+
+
+
+
+            
+
+            # Ball collision with players    
+            ball_collided = pg.sprite.spritecollide(playerleft, ball_sprites, False)
+            ball_collided = pg.sprite.spritecollide(playerright, ball_sprites, False)
+
+            screen.fill(WHITE)
+
+            all_sprites.draw(screen)
+            # active_sprite_list.draw(screen)
+
+            # Update the screen with anything new
+            pg.display.flip()
+
+            # --- Tick the Clock
+            clock.tick(60)  # 60 fps
 
         pg.quit()
 
