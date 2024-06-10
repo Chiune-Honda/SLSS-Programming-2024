69a50e2a603114e5d44e9e384092713237c2fba7
Chiune <chiunehonda@gmail.com>
2024-06-10 12:50:18 -0700

    fhmghmjhjmbmnbjm

--- main.py
+++ main.py
@@ -27,10 +27,15 @@ class PlayerL(pg.sprite.Sprite):
 
         # self.image = pg.Surface((25, 120))
         # self.image.fill(BLACK)
-        self.image = pg.image.load("./images/crayonshinchanbestversionleft.png")
-        self.image = pg.transform.scale(
-            self.image, (self.image.get_width() // 2.75, self.image.get_height() // 2.75)
-        )
+        # self.image = pg.image.load("./images/crayonshinchanbestversionleft.png")
+        # self.image = pg.transform.scale(
+        #     self.image, (self.image.get_width() // 2.75, self.image.get_height() // 2.75)
+        # )
+
+        self.radius = 20  # Radius of the ball
+        self.diameter = self.radius * 2
+        self.image = pg.Surface((self.diameter, self.diameter), pg.SRCALPHA)
+        pg.draw.circle(self.image, BLUE, (self.radius, self.radius), self.radius)
 
         self.rect = self.image.get_rect()
 
@@ -65,13 +70,12 @@ class PlayerL(pg.sprite.Sprite):
             self.rect.bottom = HEIGHT
         if self.rect.top < 0:
             self.rect.top = 0
-    def update(self):
         """Keep the player in the screen"""
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > WIDTH / 2:
             self.rect.right = WIDTH / 2
-
+            
 
 class PlayerR(pg.sprite.Sprite):
     def __init__(self):
@@ -82,12 +86,15 @@ class PlayerR(pg.sprite.Sprite):
         # self.image = pg.Surface((25, 120))
         # self.image.fill(BLACK)
 
-        self.image = pg.image.load("./images/crayon-shin-chan-right.png")
-        self.image = pg.transform.scale(
-            self.image, (self.image.get_width() // 1.75, self.image.get_height() // 1.75)
-        )
+        # self.image = pg.image.load("./images/crayon-shin-chan-right.png")
+        # self.image = pg.transform.scale(
+        #     self.image, (self.image.get_width() // 1.75, self.image.get_height() // 1.75)
+        # )
 
-
+        self.radius = 20  # Radius of the ball
+        self.diameter = self.radius * 2
+        self.image = pg.Surface((self.diameter, self.diameter), pg.SRCALPHA)
+        pg.draw.circle(self.image, RED, (self.radius, self.radius), self.radius)
 
         self.rect = self.image.get_rect()
 
@@ -122,13 +129,39 @@ class PlayerR(pg.sprite.Sprite):
             self.rect.bottom = HEIGHT
         if self.rect.top < 0:
             self.rect.top = 0
-    def update(self):
         """Keep the player in the screen"""
         if self.rect.left < WIDTH / 2:
             self.rect.left = WIDTH / 2
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
 
+class GoalR(pg.sprite.Sprite):
+     def __init__(self):
+        super().__init__()
+
+        self.image = pg.Surface((15, 130))
+        self.image.fill(BLACK)
+
+        self.rect = self.image.get_rect()
+
+        # Place goal in the middle of the side of the screen
+
+        self.rect.right = WIDTH
+        self.rect.y = HEIGHT // 2 - 50
+
+class GoalL(pg.sprite.Sprite):
+     def __init__(self):
+        super().__init__()
+
+        self.image = pg.Surface((15, 130))
+        self.image.fill(BLACK)
+
+        self.rect = self.image.get_rect()
+
+        # Place goal in the middle of the side of the screen
+
+        self.rect.left = 0
+        self.rect.y = HEIGHT // 2 - 50
 
 
 
@@ -138,31 +171,14 @@ class Ball(pg.sprite.Sprite):
         # Super class constructor
         super().__init__()
 
-        # Draw a circle inside of it
-
-
-
         self.radius = 15  # Radius of the ball
         self.diameter = self.radius * 2
         self.image = pg.Surface((self.diameter, self.diameter), pg.SRCALPHA)
         pg.draw.circle(self.image, BLACK, (self.radius, self.radius), self.radius)
 
-        self.rect = self.image.get_rect()
-        
-        # self.rect.x, self.rect.y = WIDTH // 2 - self.radius, HEIGHT // 2 - self.rad
-        # self.image = pygame.Surface((30, 30))
-        # # TODO: Make a circle
-        # pygame.draw.circle(self.image, BLACK, (self.image.get_width() // 2, self.image.get_height() // 2), self.image.get_width() // 10)
-        # # ball = pygame.Rect(WIDTH // 2 - 15, HEIGHT // 2 - 15, 30, 30)
-
         self.ball_speed = [5, 5]
 
         self.rect = self.image.get_rect()
-
-    def update(self):
-        # Move ball
-        self.rect.x += self.ball_speed[0]
-        self.rect.y += self.ball_speed[1]
 
     def update(self):
         # Move ball
@@ -175,138 +191,112 @@ class Ball(pg.sprite.Sprite):
         if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
             self.ball_speed[1] *= -1
 
+def reset_positions(playerleft, playerright, goalright, goalleft, ball):
+    playerleft.rect.right
+
 # Game loop
 def start():
+    pg.init()
+
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+
+    font = pg.font.SysFont("Futura", 24)
+    score = 0
+
+    playerleft = PlayerL()
+    playerright = PlayerR()
+    goalright = GoalR()
+    goalleft = GoalL()
+    ball = Ball()
+
+    all_sprites = pg.sprite.Group()
+    ball_sprites = pg.sprite.Group()
+    goalleft_sprites = pg.sprite.Group()
+    goalright_sprites = pg.sprite.Group()
+
+    all_sprites.add(playerleft)
+    all_sprites.add(playerright)
+    all_sprites.add(goalright)
+    all_sprites.add(goalleft)
+    all_sprites.add(ball)
+
+    goalleft_sprites.add(goalleft)
+    goalright_sprites.add(goalright)
+
+    ball_sprites.add(ball)
+
     
-        pg.init()
-
-        # --Game State Variables--
-        screen = pg.display.set_mode(SCREEN_SIZE)
-        done = False
-        clock = pg.time.Clock()
-
-        playerleft = PlayerL()
-        playerright = PlayerR()
-
-        all_sprites = pg.sprite.Group()
-        ball_sprites = pg.sprite.Group()
-
-        all_sprites.add(playerleft)
-        all_sprites.add(playerright)
-
-        ball = Ball()
-
-        all_sprites.add(ball)
-        ball_sprites.add(ball)
-
-        active_sprite_list = pg.sprite.Group()
-        # active_sprite_list.add(playerleft)
-        # active_sprite_list.add(playerright)
-        
-        while not done:
+    while not done:
         # --- Event Listener
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    done = True
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
 
-                # if event.type == pg.KEYDOWN:
-                #     if event.key == pg.K_DOWN:
-                #         playerright.go_down()
-                #     if event.key == pg.K_LEFT:
-                #         playerright.go_left()
-                #     if event.key == pg.K_RIGHT:
-                #         playerright.go_right()
-                #     if event.key == pg.K_UP:
-                #         playerright.go_up()
- 
-                #     # if event.key== pg.K_w:
-                #     #     playerleft.go_up()
-                #     # if event.key == pg.K_a:
-                #     #     playerright.go_left()
-                #     # if event.key == pg.K_d:
-                #     #     playerright.go_right()
-                #     # if event.key == pg.K_s:
-                #     #     playerright.go_down()
+        all_sprites.update()
 
-                # if event.type == pg.KEYUP:
-                #     if event.key == pg.K_LEFT and playerleft.change_x < 0:
-                #         playerleft.stop()
-                #     if event.key == pg.K_RIGHT and playerleft.change_x > 0:
-                #         playerleft.stop()
-                #     if event.key == pg.K_DOWN and playerleft.change_y < 0:
-                #         playerleft.stop()
-                #     if event.key == pg.K_UP and playerleft.change_y > 0:
-                #         playerleft.stop()
-                
-                # if event.type == pg.KEYUP:
-                #     if event.key == pg.K_LEFT and playerright.change_x < 0:
-                #         playerright.stop()
-                #     if event.key == pg.K_RIGHT and playerright.change_x > 0:
-                #         playerright.stop()
-                #     if event.key == pg.K_DOWN and playerright.change_y < 0:
-                #         playerright.stop()
-                #     if event.key == pg.K_UP and playerright.change_y > 0:
-                #         playerright.stop()
-                    
-            # Key and controls
-            keys_pressed = pg.key.get_pressed()
-
-            if keys_pressed[pg.K_w]:
-                playerleft.go_up()
-            if keys_pressed[pg.K_s]:
-                playerleft.go_down()
-            if keys_pressed[pg.K_a]:
-                playerleft.go_left()
-            if keys_pressed[pg.K_d]:
-                playerleft.go_right()
+        goalleft_collided = pg.sprite.spritecollide(ball, goalleft_sprites, True)
+        goalright_collided = pg.sprite.spritecollide(ball, goalright_sprites, True)
 
 
-            if keys_pressed[pg.K_UP]:
-                playerright.go_up()
-            if keys_pressed[pg.K_DOWN]:
-                playerright.go_down()
-            if keys_pressed[pg.K_LEFT]:
-                playerright.go_left()
-            if keys_pressed[pg.K_RIGHT]:
-                playerright.go_right()
+        for goalleft in goalleft_collided:
+        # Increase the score by 10
+            score += 1
+            print(f"Score: {score}")
+            reset_positions()
+
+        for goalright in goalright_collided:
+        # Increase the score by 10
+            score += 1
+            print(f"Score: {score}")
+
+        
+                        
+        # Key and controls
+        keys_pressed = pg.key.get_pressed()
+
+        if keys_pressed[pg.K_w]:
+            playerleft.go_up()
+        if keys_pressed[pg.K_s]:
+            playerleft.go_down()
+        if keys_pressed[pg.K_a]:
+            playerleft.go_left()
+        if keys_pressed[pg.K_d]:
+            playerleft.go_right()
 
 
-            if ball.rect.colliderect(playerleft):
-                if ball.ball_speed[0] < 0 and ball.rect.left < playerleft.rect.right:
-                    ball.rect.left = playerleft.rect.right
-                    ball.ball_speed[0] *= -1
-                if ball.ball_speed[0] > 0 and ball.rect.right > playerleft.rect.left:
-                    ball.rect.right = playerleft.rect.left
-                    ball.ball_speed[0] *= -1
-                # ball.ball_speed[1] *= -1
-
-            # ball hits the left side of playerleft
-            # for y in range(playerleft.rect.top, playerleft.rect.bottom):
-            #     if ball.rect.collidepoint(playerleft.rect.left, y):
-            #         ball.ball_speed[0] *= -1
-
-            #         print("collided")
-
-            if ball.rect.colliderect(playerright):
-                ball.ball_speed[0] *= -1
+        if keys_pressed[pg.K_UP]:
+            playerright.go_up()
+        if keys_pressed[pg.K_DOWN]:
+            playerright.go_down()
+        if keys_pressed[pg.K_LEFT]:
+            playerright.go_left()
+        if keys_pressed[pg.K_RIGHT]:
+            playerright.go_right()
 
 
-            all_sprites.update()
+        if ball.rect.colliderect(playerleft):
+            ball.ball_speed[0] *= -1
 
-            screen.fill(WHITE)
+        if ball.rect.colliderect(playerright):
+            ball.ball_speed[0] *= -1
 
-            
-            all_sprites.draw(screen)
-            #
-            # tive_sprite_list.draw(screen)
+        screen.fill(WHITE)
 
-            # Update the screen with anything new
-            pg.display.flip()
+        score_image = font.render(f"Score: {score}", True, BLACK)
+        all_sprites.draw(screen)
 
-            # --- Tick the Clock
-            clock.tick(60)  # 60 fps
+        # "Blit" the surface on the screen
+        screen.blit(score_image, (5, 5))
 
-        pg.quit()
+        all_sprites.draw(screen)
+           
+        pg.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(75)  # 75 fps
 
 def main():
     start()
